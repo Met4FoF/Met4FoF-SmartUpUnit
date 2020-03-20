0b175f3e365b82fb84d30bc509110c1335782c01
Unknown <bene.see@gmail.com>
2020-03-20 08:15:34 +0000

    changed adc1 sampling time to 3 cycles to improve frequency response with out losing SNR

--- Src/adc.c
+++ Src/adc.c
@@ -55,7 +55,7 @@ void MX_ADC1_Init(void)
   */
   sConfig.Channel = ADC_CHANNEL_9;
   sConfig.Rank = ADC_REGULAR_RANK_1;
-  sConfig.SamplingTime = ADC_SAMPLETIME_15CYCLES ;
+  sConfig.SamplingTime = ADC_SAMPLETIME_3CYCLES ;
   if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
   {
     Error_Handler();
