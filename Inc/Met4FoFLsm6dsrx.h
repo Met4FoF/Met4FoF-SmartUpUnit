895480e0847425627a13c4be9b1838150ea3979d
Benedikt Seeger <benedikt.seeger@ptb.de>
2022-10-24 10:40:19 +0200

    first version of LSM6DSRX Driver finished

--- Inc/Met4FoFLsm6dsrx.h
+++ Inc/Met4FoFLsm6dsrx.h
@@ -20,30 +20,65 @@
 #include "Met4FoFSensor.h"
 
 #include "lsm6dsrx_reg.h"
+#include "cmsis_os.h"//for OsDelay in setUp Function
 
 class Met4FoFLsm6dsrx: public Met4FoFSensor
 {
-  public:
+public:
+	enum outPutDatarate
+	{
+	  ODR_OFF    = 0,
+	  ODR_12Hz5  = 1,
+	  ODR_26Hz   = 2,
+	  ODR_52Hz   = 3,
+	  ODR_104Hz  = 4,
+	  ODR_208Hz  = 5,
+	  ODR_416Hz  = 6,
+	  ODR_833Hz  = 7,
+	  ODR_1666Hz = 8,
+	  ODR_332Hz = 9,
+	  ODR_6667Hz = 10
+	};
   Met4FoFLsm6dsrx(GPIO_TypeDef* SPICSTypeDef, uint16_t SPICSPin,SPI_HandleTypeDef* spiIfaceHandle,uint32_t BaseID);
   int getData(DataMessage * Message,uint64_t RawTimeStamp);
   int getDescription(DescriptionMessage * Message,DescriptionMessage_DESCRIPTION_TYPE DESCRIPTION_TYPE);
   uint32_t getSampleCount();
   void increaseCaptureCountWORead(){_SampleCount++;return ;};
   int setBaseID(uint32_t BaseID);
-  float getNominalSamplingFreq(){return 0.0;};
+  float getNominalSamplingFreq(){return _NominalSamplingFreq;};
   int setUp();
   private:
   int32_t platform_write(void *handle, uint8_t reg, const uint8_t *bufp,uint16_t len);
   int32_t platform_read(void *handle, uint8_t reg, uint8_t *bufp,uint16_t len);
+  int setODR(Met4FoFLsm6dsrx::outPutDatarate odr);
+  int setAccFS(lsm6dsrx_fs_xl_t accFullScale);
+  int setGyroFS(lsm6dsrx_fs_g_t gyroFullScale);
   GPIO_TypeDef* _SPICSPort;
   uint16_t _SPICSPin;
   SPI_HandleTypeDef* _spi;
+
   uint32_t _ID;
   uint32_t _BaseID;
   uint16_t _SetingsID;
   uint32_t _SampleCount=0;
+  float _NominalSamplingFreq=-1;
+
   stmdev_ctx_t _dev_ctx;
-  uint8_t _whoamI;
+  uint8_t _connectionInitRetys=10;
+  static const uint8_t _connectionInitDelayMs=10;
+  uint8_t _whoamI,_rst;
+  // Internal register configurations
+  lsm6dsrx_pin_int1_route_t _int1_route;
+  lsm6dsrx_pin_int2_route_t _int2_route;
+  Met4FoFLsm6dsrx::outPutDatarate _odr=ODR_104Hz;
+  lsm6dsrx_fs_xl_t _accFullScaleSet=LSM6DSRX_16g;
+  float _ACCFSScaleFactor=0.488f;
+  lsm6dsrx_fs_g_t _gyroFullScaleSet=LSM6DSRX_4000dps;
+  float _GyroFSScaleFactor=140.0f;
+  float _TempFSScaleFactor=1.0/256.0f;
+  float _TempScaleOffset=25.0f;
+
+
 
 };
 
--- Src/Met4FoFLsm6dsrx.cpp
+++ Src/Met4FoFLsm6dsrx.cpp
@@ -35,14 +35,159 @@ uint32_t Met4FoFLsm6dsrx::getSampleCount(){
 	return _SampleCount;
 }
 
+int Met4FoFLsm6dsrx::setODR(Met4FoFLsm6dsrx::outPutDatarate odr) {
 
-int Met4FoFLsm6dsrx::setUp(){
-	lsm6dsrx_device_id_get(&_dev_ctx, &_whoamI);
-	if (_whoamI != LSM6DSRX_ID){
-		return -1;
+	int32_t retSetAccODR=lsm6dsrx_xl_data_rate_set(&_dev_ctx,(lsm6dsrx_odr_xl_t) odr);//ACC
+	int32_t retSetGyroODR=lsm6dsrx_gy_data_rate_set(&_dev_ctx,(lsm6dsrx_odr_g_t) odr);//Gyro
+	if(retSetAccODR==0 and  retSetGyroODR==0)
+	{
+
+	  switch(odr){
+	  case ODR_OFF:
+		  _NominalSamplingFreq=0.0f;
+		  break;
+	  case ODR_12Hz5:
+		  _NominalSamplingFreq=12.5f;
+		  break;
+	  case ODR_26Hz:
+		  _NominalSamplingFreq=26.0f;
+		  break;
+	  case ODR_52Hz:
+		  _NominalSamplingFreq=52.0f;
+		  break;
+	  case ODR_104Hz:
+		  _NominalSamplingFreq=104.0f;
+		  break;
+	  case ODR_208Hz:
+		  _NominalSamplingFreq=208.0f;
+		  break;
+	  case ODR_416Hz:
+		  _NominalSamplingFreq=416.0f;
+		  break;
+	  case ODR_833Hz:
+		  _NominalSamplingFreq=833.0f;
+		  break;
+	  case ODR_1666Hz:
+		  _NominalSamplingFreq=1666.0f;
+		  break;
+	  case ODR_332Hz:
+		  _NominalSamplingFreq=332.0f;
+		  break;
+	  case ODR_6667Hz:
+		  _NominalSamplingFreq=6667.0f;
+		  break;
+	  default:
+		  _NominalSamplingFreq=NAN;
+		  break;
+	  }
+	  return 0;
 	}
 	else
-		return 0;
+	{
+		return retSetAccODR+retSetGyroODR*256;
+	}
+}
+
+int Met4FoFLsm6dsrx::setAccFS(lsm6dsrx_fs_xl_t accFullScale){
+	 int ret=lsm6dsrx_xl_full_scale_set(&_dev_ctx, accFullScale);//Acc
+	 if (ret==0)
+	 {
+		  _accFullScaleSet=accFullScale;
+		  switch(accFullScale){
+		  case LSM6DSRX_2g:
+			  _ACCFSScaleFactor=0.061f;
+			  break;
+		  case LSM6DSRX_4g:
+			  _ACCFSScaleFactor=0.122f;
+			  break;
+		  case LSM6DSRX_8g:
+			  _ACCFSScaleFactor=0.244f;
+			  break;
+		  case LSM6DSRX_16g:
+			  _ACCFSScaleFactor=0.488f;
+			  break;
+		  default:
+			  _ACCFSScaleFactor=NAN;
+			  break;
+		  }
+	 }
+return ret;
+}
+
+int Met4FoFLsm6dsrx::setGyroFS(lsm6dsrx_fs_g_t gyroFullScale){
+		 int ret=lsm6dsrx_gy_full_scale_set(&_dev_ctx, gyroFullScale);//Acc
+		 if (ret==0)
+		 {
+			  _gyroFullScaleSet=gyroFullScale;
+			  switch(gyroFullScale){
+			  case LSM6DSRX_125dps:
+				  _GyroFSScaleFactor=4.375f;
+				  break;
+			  case LSM6DSRX_250dps:
+				  _GyroFSScaleFactor=8.75f;
+				  break;
+			  case LSM6DSRX_500dps:
+				  _GyroFSScaleFactor=17.50f;
+				  break;
+			  case LSM6DSRX_1000dps:
+				  _GyroFSScaleFactor=35.0f;
+				  break;
+			  case LSM6DSRX_2000dps:
+				  _GyroFSScaleFactor=70.0f;
+				  break;
+			  case LSM6DSRX_4000dps:
+				  _GyroFSScaleFactor=140.0f;
+				  break;
+			  default:
+				  _GyroFSScaleFactor=NAN;
+				  break;
+			  }
+		 }
+	return ret;
+	}
+
+
+int Met4FoFLsm6dsrx::setUp(){
+	bool connectionSuccsesfull=false;
+	unsigned int retrysSoFar=0;
+	while (connectionSuccsesfull==false and retrysSoFar<_connectionInitRetys){
+		lsm6dsrx_device_id_get(&_dev_ctx, &_whoamI);
+		if (_whoamI != LSM6DSRX_ID){
+			connectionSuccsesfull=true;
+			osDelay(_connectionInitDelayMs);
+		}
+		retrysSoFar++;
+	}
+	if (connectionSuccsesfull==false)
+	{
+		return -1;
+	}
+	  /* Restore default configuration. */
+	  lsm6dsrx_reset_set(&_dev_ctx, PROPERTY_ENABLE);
+	  do {
+	    lsm6dsrx_reset_get(&_dev_ctx, &_rst);
+	  } while (_rst);
+
+	  /* Disable I3C interface. */
+	  lsm6dsrx_i3c_disable_set(&_dev_ctx, LSM6DSRX_I3C_DISABLE);
+	  /* Enable Block Data Update. */
+	  /* Enable Block Data Update. */
+	  lsm6dsrx_block_data_update_set(&_dev_ctx, PROPERTY_ENABLE);
+	  /* Set Output Data Rate. */
+	  setODR(_odr);
+	  setAccFS(_accFullScaleSet);
+	  setGyroFS(_gyroFullScaleSet);
+	  /* Set full scale. */
+
+	  /* Enable drdy 75 μs pulse: uncomment if interrupt must be pulsed. */
+	  //lsm6dsrx_data_ready_mode_set(&dev_ctx, LSM6DSRX_DRDY_PULSED);
+
+	  lsm6dsrx_pin_int1_route_get(&_dev_ctx, &_int1_route);
+	  //TODO Debug this registers with actual sensors
+	  _int1_route.int1_ctrl.den_drdy_flag = PROPERTY_ENABLE;
+	  lsm6dsrx_pin_int1_route_set(&_dev_ctx, &_int1_route);
+	  return 0;
+
 }
 
 
@@ -60,24 +205,27 @@ int Met4FoFLsm6dsrx::getData(DataMessage * Message,uint64_t RawTimeStamp){
 	Message->time_uncertainty=(uint32_t)((RawTimeStamp & 0xFFFFFFFF00000000) >> 32);//high word
 	Message->unix_time_nsecs=(uint32_t)(RawTimeStamp & 0x00000000FFFFFFFF);// low word
 	Message->sample_number=	_SampleCount;
-	//TODO Add read sensor code
-	//readresult=Met4FoFLsm6dsrx::readSensor();
 
-	/*
-	Message->Data_01=_ax;
+	int16_t data_raw_acceleration[3];
+	int16_t data_raw_angular_rate[3];
+	int16_t data_raw_temp;
+
+	lsm6dsrx_acceleration_raw_get(&_dev_ctx, data_raw_acceleration);
+	lsm6dsrx_angular_rate_raw_get(&_dev_ctx, data_raw_angular_rate);
+	lsm6dsrx_temperature_raw_get(&_dev_ctx, &data_raw_temp);
+	Message->Data_01=(float)data_raw_acceleration[0]*_ACCFSScaleFactor;
 	Message->has_Data_02=true;
-	Message->Data_02=_ay;
+	Message->Data_02=(float)data_raw_acceleration[1]*_ACCFSScaleFactor;
 	Message->has_Data_03=true;
-	Message->Data_03=_az;
+	Message->Data_03=(float)data_raw_acceleration[2]*_ACCFSScaleFactor;
 	Message->has_Data_04=true;
-	Message->Data_04=_gx;
+	Message->Data_04=(float)data_raw_angular_rate[0]*_GyroFSScaleFactor;
 	Message->has_Data_05=true;
-	Message->Data_05=_gy;
+	Message->Data_05=(float)data_raw_angular_rate[1]*_GyroFSScaleFactor;
 	Message->has_Data_06=true;
-	Message->Data_06=_gz;
+	Message->Data_06=(float)data_raw_angular_rate[2]*_GyroFSScaleFactor;
 	Message->has_Data_07=true;
-	Message->Data_07=_hx;
-	*/
+	Message->Data_07=(float)data_raw_temp*_TempFSScaleFactor+_TempScaleOffset;
 	return readresult;
 }
 
@@ -147,7 +295,7 @@ int Met4FoFLsm6dsrx::getDescription(DescriptionMessage * Message,DescriptionMess
 	}
 	if(DESCRIPTION_TYPE==DescriptionMessage_DESCRIPTION_TYPE_RESOLUTION)
 	{
-		/*
+
 		Message->has_f_Data_01=true;
 		Message->has_f_Data_02=true;
 		Message->has_f_Data_03=true;
@@ -155,19 +303,19 @@ int Met4FoFLsm6dsrx::getDescription(DescriptionMessage * Message,DescriptionMess
 		Message->has_f_Data_05=true;
 		Message->has_f_Data_06=true;
 		Message->has_f_Data_07=true;
-		Message->f_Data_01=65536;
+		Message->f_Data_01=65536;//All Channels 16 Bit
 		Message->f_Data_02=65536;
 		Message->f_Data_03=65536;
 		Message->f_Data_04=65536;
 		Message->f_Data_05=65536;
 		Message->f_Data_06=65536;
-		Message->f_Data_07=65520;
-		*/
+		Message->f_Data_07=65536;
+
 	}
 	//TODO add min and max scale values as calls member vars so they have not to be calculated all the time
 	if(DESCRIPTION_TYPE==DescriptionMessage_DESCRIPTION_TYPE_MIN_SCALE)
 	{
-		/*
+
 		Message->has_f_Data_01=true;
 		Message->has_f_Data_02=true;
 		Message->has_f_Data_03=true;
@@ -175,18 +323,18 @@ int Met4FoFLsm6dsrx::getDescription(DescriptionMessage * Message,DescriptionMess
 		Message->has_f_Data_05=true;
 		Message->has_f_Data_06=true;
 		Message->has_f_Data_07=true;
-		Message->f_Data_01=accMIN;
-		Message->f_Data_02=accMIN;
-		Message->f_Data_03=accMIN;
-		Message->f_Data_04=gyrMIN;
-		Message->f_Data_05=gyrMIN;
-		Message->f_Data_06=gyrMIN;
-		Message->f_Data_07=tempMIN;
-		*/
+		Message->f_Data_01=-32768.0f*_ACCFSScaleFactor;
+		Message->f_Data_02=-32768.0f*_ACCFSScaleFactor;
+		Message->f_Data_03=-32768.0f*_ACCFSScaleFactor;
+		Message->f_Data_04=-32768.0f*_GyroFSScaleFactor;
+		Message->f_Data_05=-32768.0f*_GyroFSScaleFactor;
+		Message->f_Data_06=-32768.0f*_GyroFSScaleFactor;
+		Message->f_Data_07=-32768.0f/25.0f+25.0f;
+
 	}
 	if(DESCRIPTION_TYPE==DescriptionMessage_DESCRIPTION_TYPE_MAX_SCALE)
 	{
-		/*
+
 		Message->has_f_Data_01=true;
 		Message->has_f_Data_02=true;
 		Message->has_f_Data_03=true;
@@ -194,14 +342,14 @@ int Met4FoFLsm6dsrx::getDescription(DescriptionMessage * Message,DescriptionMess
 		Message->has_f_Data_05=true;
 		Message->has_f_Data_06=true;
 		Message->has_f_Data_07=true;
-		Message->f_Data_01=accMAX;
-		Message->f_Data_02=accMAX;
-		Message->f_Data_03=accMAX;
-		Message->f_Data_04=gyrMAX;
-		Message->f_Data_05=gyrMAX;
-		Message->f_Data_06=gyrMAX;
-		Message->f_Data_7=tempMAX;
-		*/
+		Message->f_Data_01=32767.0f*_ACCFSScaleFactor;
+		Message->f_Data_02=32767.0f*_ACCFSScaleFactor;
+		Message->f_Data_03=32767.0f*_ACCFSScaleFactor;
+		Message->f_Data_04=32767.0f*_GyroFSScaleFactor;
+		Message->f_Data_05=32767.0f*_GyroFSScaleFactor;
+		Message->f_Data_06=32767.0f*_GyroFSScaleFactor;
+		Message->f_Data_07=32767.0f/25.0f+25.0f;
+
 	}
 	if(DESCRIPTION_TYPE==DescriptionMessage_DESCRIPTION_TYPE_HIERARCHY)
 	{
