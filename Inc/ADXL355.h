da902d8ce8f2f1dec5a70e969c58ea886f972ded
Benedikt Seeger <bene.see@gmail.com>
2023-05-10 07:58:04 +0200

    started with adxl355 implementation

---
+++ Inc/ADXL355.h
@@ -0,0 +1,214 @@
+/*
+ * https://github.com/bolderflight/ADXL355
+ADXL355.h
+Brian R Taylor
+brian.taylor@bolderflight.com
+
+Copyright (c) 2017 Bolder Flight Systems
+changed by B. Seeger to be usable at stm32 with HAl 2019
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software 
+and associated documentation files (the "Software"), to deal in the Software without restriction, 
+including without limitation the rights to use, copy, modify, merge, publish, distribute, 
+sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is 
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or 
+substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING 
+BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND 
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, 
+DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+
+
+#ifndef ADXL355_h
+#define ADXL355_h
+
+#include "spi.h"
+#include "gpio.h"
+#include "stm32f7xx_hal.h"
+
+// for osDelay instead of HAL_delay
+#include "cmsis_os.h"
+
+// memcpy and interger types
+#include <stdint.h>
+#include <cstring>
+#include <math.h>
+#include <stdint.h>
+
+#include "pb.h"
+#include "message.pb.h"
+#include "Met4FoFSensor.h"
+
+
+#define SPI_TIMEOUT 100U
+//extern DescriptionMessage empty_DescriptionMessage;
+//extern DataMessage empty_DataMessage;
+
+class ADXL355: public Met4FoFSensors::Met4FoFSensor {
+  public:
+	enum adxl355_hpf_corner {
+		ADXL355_HPF_OFF,
+		ADXL355_HPF_24_7,
+		ADXL355_HPF_6_2084,
+		ADXL355_HPF_1_5545,
+		ADXL355_HPF_0_3862,
+		ADXL355_HPF_0_0954,
+		ADXL355_HPF_0_0238
+	};
+
+	enum adxl355_odr_lpf {
+		ADXL355_ODR_4000HZ,
+		ADXL355_ODR_2000HZ,
+		ADXL355_ODR_1000HZ,
+		ADXL355_ODR_500HZ,
+		ADXL355_ODR_250HZ,
+		ADXL355_ODR_125HZ,
+		ADXL355_ODR_62_5HZ,
+		ADXL355_ODR_31_25HZ,
+		ADXL355_ODR_15_625HZ,
+		ADXL355_ODR_7_813HZ,
+		ADXL355_ODR_3_906HZ
+	};
+
+	enum adxl355_range {
+		ADXL355_RANGE_2G = 1,
+		ADXL355_RANGE_4G = 2,
+		ADXL355_RANGE_8G = 3,
+	};
+
+	enum adxl355_int_pol {
+		ADXL355_INT_ACTIVE_LOW = 0,
+		ADXL355_INT_ACTIVE_HIGH = 1
+	};
+	struct _adxl355_int_mask {
+		uint8_t RDY_EN1 : 1;
+		uint8_t FULL_EN1 : 1;
+		uint8_t OVR_EN1 : 1;
+		uint8_t ACT_EN1 : 1;
+		uint8_t RDY_EN2 : 1;
+		uint8_t FULL_EN2 : 1;
+		uint8_t OVR_EN2 : 1;
+		uint8_t ACT_EN2 : 1;
+	};
+
+	union adxl355_int_mask {
+		struct _adxl355_int_mask fields;
+		uint8_t value;
+	};
+
+	struct _adxl355_sts_reg_flags {
+		uint8_t DATA_RDY : 1;
+		uint8_t FIFO_FULL : 1;
+		uint8_t FIFO_OVR : 1;
+		uint8_t Activity : 1;
+		uint8_t NVM_BUSY : 1;
+		uint8_t reserved : 3;
+	};
+
+	union adxl355_sts_reg_flags {
+		struct _adxl355_sts_reg_flags fields;
+		uint8_t value;
+	};
+
+	struct _adxl355_act_en_flags {
+		uint8_t ACT_X    : 1;
+		uint8_t ACT_Y    : 1;
+		uint8_t ACT_Z    : 1;
+		uint8_t reserved : 4;
+	};
+
+	union adxl355_act_en_flags {
+		struct _adxl355_act_en_flags fields;
+		uint8_t value;
+	};
+
+	struct adxl355_frac_repr {
+		int64_t integer;
+		int32_t fractional;
+	} ;
+
+    ADXL355(GPIO_TypeDef* SPICSTypeDef, uint16_t SPICSPin,SPI_HandleTypeDef* ADXL355spi,uint32_t BaseID);
+    ~ADXL355();
+    int begin();
+    void setAccSelfTest(uint8_t SelftestStatus);
+    void setGyroSelfTest(uint8_t SelftestStatus);
+    void setScaleFactor(adxl355_range scaleFactor);
+    void setHPFCorner(adxl355_hpf_corner hpfCorner);
+    void setLPFCorner(adxl355_odr_lpf lpfFreq);
+    int readSensor();
+    int getData(DataMessage * Message,uint64_t RawTimeStamp);
+    int getDescription(DescriptionMessage * Message,DescriptionMessage_DESCRIPTION_TYPE DESCRIPTION_TYPE);
+    void increaseCaptureCountWORead(){_SampleCount++;return ;};
+    float getNominalSamplingFreq();
+    float accX=0.0;
+    float accY=0.0;
+    float accZ=0.0;
+    float temp=0.0;
+    uint32_t accRawX=0;
+    uint32_t accRawY=0;
+    uint32_t accRawZ=0;
+    uint32_t tempRaw=0;
+    uint32_t _SampleCount=0;
+
+  protected:
+    adxl355_odr_lpf lpfSeting=ADXL355_ODR_4000HZ;
+    adxl355_hpf_corner hpfSeting=ADXL355_HPF_OFF;
+    adxl355_range scalfactoSetting=ADXL355_RANGE_2G;
+    GPIO_TypeDef* _SPICSTypeDef;
+    uint16_t _SPICSPin;
+    SPI_HandleTypeDef* _ADXL355spi;
+    // private functions
+    int writeRegister(uint8_t subAddress, uint8_t data);
+    int readRegisters(uint8_t subAddress, uint8_t count, uint8_t* dest);
+    float convertTempReading(uint16_t);
+    float convertACCReading(uint32_t);
+    float accScalefacotr=0.00003824593;
+    uint8_t buffer[512];
+    /* ADXL355 registers addresses */
+    const uint8_t DEVID_AD =                0x00;
+    const uint8_t DEVID_MST =               0x01;
+    const uint8_t PARTID     =              0x02;
+    const uint8_t REVID =                   0x03;
+    const uint8_t STATUS =                  0x04;
+    const uint8_t FIFO_ENTRIES =            0x05;
+    const uint8_t TEMP2 =                   0x06;
+    const uint8_t TEMP1  =                  0x07;
+    const uint8_t XDATA3 =                  0x08;
+    const uint8_t XDATA2 =                  0x09;
+    const uint8_t XDATA1 =                  0x0A;
+    const uint8_t YDATA3 =                  0x0B;
+    const uint8_t YDATA2 =                  0x0C;
+    const uint8_t YDATA1 =                  0x0D;
+    const uint8_t ZDATA3 =                  0x0E;
+    const uint8_t ZDATA2 =                  0x0F;
+    const uint8_t ZDATA1 =                  0x10;
+    const uint8_t FIFO_DATA =               0x11;
+    const uint8_t OFFSET_X_H =              0x1E;
+    const uint8_t OFFSET_X_L =              0x1F;
+    const uint8_t OFFSET_Y_H =              0x20;
+    const uint8_t OFFSET_Y_L =              0x21;
+    const uint8_t OFFSET_Z_H =              0x22;
+    const uint8_t OFFSET_Z_L =              0x23;
+    const uint8_t ACT_EN =                  0x24;
+    const uint8_t ACT_THRESH_H =            0x25;
+    const uint8_t ACT_THRESH_L =            0x26;
+    const uint8_t ACT_COUNT =               0x27;
+    const uint8_t FILTER =                  0x28;
+    const uint8_t FIFO_SAMPLES =            0x29;
+    const uint8_t INT_MAP =                 0x2A;
+    const uint8_t SYNC =                    0x2B;
+    const uint8_t RANGE =                   0x2C;
+    const uint8_t POWER_CTL =               0x2D;
+    const uint8_t SELF_TEST =               0x2E;
+    const uint8_t RESET =                   0x2F;
+};
+
+
+
+#endif
---
+++ Src/ADXL355.cpp
@@ -0,0 +1,214 @@
+/*
+ *
+ * https://github.com/bolderflight/ADXL355
+ADXL355.cpp
+Brian R Taylor
+brian.taylor@bolderflight.com
+
+Copyright (c) 2017 Bolder Flight Systems
+changed by B. Seeger to be usable at stm32 with HAl 2019
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software 
+and associated documentation files (the "Software"), to deal in the Software without restriction, 
+including without limitation the rights to use, copy, modify, merge, publish, distribute, 
+sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is 
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or 
+substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING 
+BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND 
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, 
+DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+#include "ADXL355.h"
+
+/* ADXL355 object, input the SPI bus and chip select pin */
+ADXL355::ADXL355(GPIO_TypeDef* SPICSTypeDef, uint16_t SPICSPin,SPI_HandleTypeDef* ADXL355spi,uint32_t BaseID):
+	Met4FoFSensor::Met4FoFSensor(BaseID){
+	_SPICSTypeDef=SPICSTypeDef;
+    _SPICSPin=SPICSPin;
+    _ADXL355spi=ADXL355spi;
+	_SetingsID=0;
+	_ID=_baseID+(uint32_t)_SetingsID;
+	Met4FoFSensors::listMet4FoFSensors.push_back((Met4FoFSensors::Met4FoFSensor *)this);
+}
+
+ADXL355::~ADXL355()
+{
+	Met4FoFSensors::listMet4FoFSensors.remove((Met4FoFSensors::Met4FoFSensor *)this);
+}
+
+/* starts communication with the MPU-9250 */
+int ADXL355::begin(){
+
+	  return 1;
+	}
+
+
+
+/* reads the most current data from ADXL355 and stores in buffer */
+int ADXL355::readSensor() {
+  // grab the data from the ADXL355
+  if (readRegisters(TEMP2, 11, buffer) < 0) {
+    return 0;
+  }
+  // combine into 16 bit values
+  tempRaw =  ((uint32_t)buffer[0] << 8 ) |  buffer[1];
+  accRawX =  ((uint32_t)buffer[2] << 16) | ((uint32_t)buffer[3] << 8) | (uint32_t)buffer[4];
+  accRawY =  ((uint32_t)buffer[5] << 16) | ((uint32_t)buffer[6] << 8) | (uint32_t)buffer[7];
+  accRawZ =  ((uint32_t)buffer[8] << 16) | ((uint32_t)buffer[9] << 8) | (uint32_t)buffer[10];
+  // transform and convert to float values
+  accX=convertACCReading(accRawX);
+  accY=convertACCReading(accRawY);
+  accZ=convertACCReading(accRawZ);
+  temp=convertTempReading(tempRaw);
+  return 1;
+}
+
+
+/* writes a byte to ADXL355 register given a register address and data */
+int ADXL355::writeRegister(uint8_t address, uint8_t data){
+  /* write data to device */
+
+  uint8_t buffer[2] = {address, data };
+  HAL_GPIO_WritePin(_SPICSTypeDef, _SPICSPin, GPIO_PIN_RESET);
+  HAL_SPI_Transmit(_ADXL355spi, buffer, 2, SPI_TIMEOUT);
+  HAL_GPIO_WritePin(_SPICSTypeDef, _SPICSPin, GPIO_PIN_SET);
+  /* read back the register */
+  readRegisters(address,1,buffer);
+  /* check the read back register against the written register */
+  if(buffer[0] == data) {
+    return 1;
+  }
+  else{
+    return -1;
+  }
+}
+
+/* reads registers from ADXL355 given a starting register address, number of bytes, and a pointer to store data */
+int ADXL355::readRegisters(uint8_t Address, uint8_t count, uint8_t* dest){
+    // begin the transaction
+  	int retVal=0;
+  	uint8_t tx[count+1]={0};
+  	uint8_t rx[count+1]={0};
+  	tx[0] = Address;
+  	HAL_GPIO_WritePin(_SPICSTypeDef, _SPICSPin, GPIO_PIN_RESET);
+  	if(HAL_SPI_TransmitReceive(_ADXL355spi,tx, rx, count+1, SPI_TIMEOUT)==HAL_OK)
+  	{
+  		retVal=1;
+  	}
+  	HAL_GPIO_WritePin(_SPICSTypeDef, _SPICSPin, GPIO_PIN_SET);
+  	memcpy(dest, &rx[1], count);
+  return retVal;
+}
+
+
+
+int ADXL355::getData(DataMessage * Message,uint64_t RawTimeStamp){
+	int result=0;
+	_SampleCount++;
+	if (Message==0){
+		return result;
+	}
+	int readresult=-1;
+	memcpy(Message,&empty_DataMessage,sizeof(DataMessage));//Copy default values into array
+	Message->id=_ID;
+	Message->unix_time=0XFFFFFFFF;
+	Message->time_uncertainty=(uint32_t)((RawTimeStamp & 0xFFFFFFFF00000000) >> 32);//high word
+	Message->unix_time_nsecs=(uint32_t)(RawTimeStamp & 0x00000000FFFFFFFF);// low word
+	Message->sample_number=	_SampleCount;
+	readresult=ADXL355::readSensor();
+	Message->Data_01=accX;
+	Message->has_Data_02=true;
+	Message->Data_02=accY;
+	Message->has_Data_03=true;
+	Message->Data_03=accZ;
+	Message->has_Data_04=true;
+	Message->Data_04=temp;
+	return readresult;
+}
+
+int ADXL355::getDescription(DescriptionMessage * Message,DescriptionMessage_DESCRIPTION_TYPE DESCRIPTION_TYPE){
+	memcpy(Message,&empty_DescriptionMessage,sizeof(DescriptionMessage));//Copy default values into array
+	int retVal=0;
+	strncpy(Message->Sensor_name,"ADXL_355\0",sizeof(Message->Sensor_name));
+	Message->id=_ID;
+	Message->Description_Type=DESCRIPTION_TYPE;
+	if(DESCRIPTION_TYPE==DescriptionMessage_DESCRIPTION_TYPE_PHYSICAL_QUANTITY)
+	{
+		Message->has_str_Data_01=true;
+		Message->has_str_Data_02=true;
+		Message->has_str_Data_03=true;
+		Message->has_str_Data_04=true;
+		strncpy(Message->str_Data_01,"X Acceleration\0",sizeof(Message->str_Data_01));
+		strncpy(Message->str_Data_02,"Y Acceleration\0",sizeof(Message->str_Data_02));
+		strncpy(Message->str_Data_03,"Z Acceleration\0",sizeof(Message->str_Data_03));
+		strncpy(Message->str_Data_04,"Temperature\0",sizeof(Message->str_Data_04));
+	}
+	if(DESCRIPTION_TYPE==DescriptionMessage_DESCRIPTION_TYPE_UNIT)
+	{
+		Message->has_str_Data_01=true;
+		Message->has_str_Data_02=true;
+		Message->has_str_Data_03=true;
+		Message->has_str_Data_04=true;
+		strncpy(Message->str_Data_01,"\\metre\\second\\tothe{-2}\0",sizeof(Message->str_Data_01));
+		strncpy(Message->str_Data_02,"\\metre\\second\\tothe{-2}\0",sizeof(Message->str_Data_02));
+		strncpy(Message->str_Data_03,"\\metre\\second\\tothe{-2}\0",sizeof(Message->str_Data_03));
+		strncpy(Message->str_Data_04,"\\degreecelsius\0",sizeof(Message->str_Data_04));
+	}
+	if(DESCRIPTION_TYPE==DescriptionMessage_DESCRIPTION_TYPE_RESOLUTION)
+	{
+		Message->has_f_Data_01=true;
+		Message->has_f_Data_02=true;
+		Message->has_f_Data_03=true;
+		Message->has_f_Data_04=true;
+		Message->f_Data_01=65536;
+		Message->f_Data_02=65536;
+		Message->f_Data_03=65536;
+		Message->f_Data_04=65536;
+	}
+	//TODO add min and max scale values as calls member vars so they have not to be calculated all the time
+	if(DESCRIPTION_TYPE==DescriptionMessage_DESCRIPTION_TYPE_MIN_SCALE)
+	{
+
+		float accMIN=convertACCReading(-524288);
+		float tempMIN= convertTempReading(-32768);
+		Message->has_f_Data_01=true;
+		Message->has_f_Data_02=true;
+		Message->has_f_Data_03=true;
+		Message->has_f_Data_04=true;
+		Message->f_Data_01=accMIN;
+		Message->f_Data_02=accMIN;
+		Message->f_Data_03=accMIN;
+		Message->f_Data_04=tempMIN;
+	}
+	if(DESCRIPTION_TYPE==DescriptionMessage_DESCRIPTION_TYPE_MAX_SCALE)
+	{
+		float accMAX=convertACCReading(524287);
+		float tempMAX=convertTempReading(32767);
+		Message->has_f_Data_01=true;
+		Message->has_f_Data_02=true;
+		Message->has_f_Data_03=true;
+		Message->has_f_Data_04=true;
+		Message->f_Data_01=accMAX;
+		Message->f_Data_02=accMAX;
+		Message->f_Data_03=accMAX;
+		Message->f_Data_04=tempMAX;
+	}
+	if(DESCRIPTION_TYPE==DescriptionMessage_DESCRIPTION_TYPE_HIERARCHY)
+	{
+		Message->has_str_Data_01=true;
+		Message->has_str_Data_02=true;
+		Message->has_str_Data_03=true;
+		Message->has_str_Data_04=true;
+		strncpy(Message->str_Data_01,"Acceleration/0\0",sizeof(Message->str_Data_01));
+		strncpy(Message->str_Data_02,"Acceleration/1\0",sizeof(Message->str_Data_02));
+		strncpy(Message->str_Data_03,"Acceleration/2\0",sizeof(Message->str_Data_03));
+		strncpy(Message->str_Data_04,"Temperature/0\0",sizeof(Message->str_Data_04));
+	}
+	return retVal;
+}
